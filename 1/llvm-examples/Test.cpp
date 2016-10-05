4864721ab6197819977f27512db56053ba3ad674
Liu Kexin <kexinliu131@gmail.com>
2016-10-05 17:03:46 +0800

    handle one single file

--- 1/llvm-examples/Test.cpp
+++ 1/llvm-examples/Test.cpp
@@ -8,8 +8,11 @@
 #include "llvm/IR/Module.h"
 #include "llvm/Support/raw_ostream.h"
 #include "llvm/Support/SourceMgr.h"
+#include <set>
 
 using namespace llvm;
+
+char** arg;
 
 Function* getFunctionByName(Module* modules[], std::string name, int length){
 	for (int i=0; i<length; i++){
@@ -20,16 +23,69 @@ Function* getFunctionByName(Module* modules[], std::string name, int length){
 	return nullptr;
 }
 
+void checkUnusedFunction(Module* modules[], int length, std::set<Function*> *checked){
+	for (int i = 0; i < length; i++){
+		Module* m = modules[i];
+		for (auto &f: *m){
+			if (checked->find(&f) == checked->end()){
+				printf("Unused function %s found in Module %s\n"
+				,f.getName().str().c_str(), arg[i+1]);
+			}
+		}
+	}
+}
+
+void processFunction(Module* modules[], std::set<Function*> *pending, std::set<Function*> *checked){
+	
+	Function* f = *(pending->begin());
+	printf ("processing function %s\n", f->getName().str().c_str());	
+
+	for (auto &BB: *f){ // For each basic block BB
+		for (auto &I: BB) {// For each instruction I
+			CallInst *Call = dyn_cast<CallInst>(&I);
+
+			if (Call == nullptr) continue;
+			Function *G = Call->getCalledFunction();
+
+			if (G == nullptr) continue;	
+
+			if (checked->find(G) != checked->end()){
+				printf("funciton %s found in checked!\n", G->getName().str().c_str());
+			}
+			
+			if (pending->find(G) != pending->end()){
+				printf("funciton %s found in pending!\n", G->getName().str().c_str());
+			} else{
+				pending->insert(G);
+			}
+		}
+	}
+
+	checked->insert(f);
+	pending->erase(f);
+}
+
+void printFunctionSet(std::set<Function*> *func_set){
+	printf("------------------printing function set-----------------\n");
+	for (std::set<Function*>::iterator it=func_set->begin(); it!=func_set->end();it++){
+		printf("%s\n", (*it) -> getName().str().c_str());
+	}
+}
+
 int main(int argc, char **argv){
 	// Step (1) Parse the given IR File
 	LLVMContext &Context = getGlobalContext();
 	SMDiagnostic Err;
 
+	std::set<Function*> pending;
+	std::set<Function*> checked;
+
 	Module* modules[argc-1];
 
 	for (int i=1; i<argc; i++){	
 
-		Module *M = ParseIRFile(argv[1], Err, Context);
+		Module *M = ParseIRFile(argv[i], Err, Context);
+
 		if (M == nullptr){
 			fprintf(stderr, "failed to read IR file %s\n", argv[1]);
 			return 1;
@@ -37,18 +93,29 @@ int main(int argc, char **argv){
 		modules[i-1] = M;
 	}
 	
+	arg = argv;
+
+	pending.insert(getFunctionByName(modules,"main",argc-1));
+
+	while (pending.size()!=0){
+		processFunction(modules, &pending, &checked);
+	}
+
+	printFunctionSet(&checked);	
+
+	printf("\n");
+	checkUnusedFunction(modules, argc-1, &checked);
+
+	/*	
 	getFunctionByName(modules,"f",argc-1);
 	getFunctionByName(modules,"Alloha!",argc-1);
-	
+
 	Module *M = modules[0];
 
 	Function *f = M->getFunction("f");
 	if (f==nullptr) printf("f not found\n");
 	else printf("f name %s\n", f->getName().str().c_str());
 
-
-
-	// Step (2) Traverse all instructions
 	for (auto &F: *M)
 		// For each function F
 		for (auto &BB: F){ // For each basic block BB
@@ -58,18 +125,19 @@ int main(int argc, char **argv){
 			}
 
 			for (auto &I: BB) {// For each instruction I
-	
+
 				CallInst *Call = dyn_cast<CallInst>(&I);
 				if (Call == nullptr) continue;
 				Function *G = Call->getCalledFunction();
 				if (G == nullptr) continue;
 				printf("Name = %s\n", G->getName().str().c_str());
+
 				
-				/*
 				BranchInst *Branch = dyn_cast<BranchInst>(&I);
 				if (Branch == nullptr) continue;
 				printf("%d\n",Branch->isConditional());
-				*/
+				 
 			}
 		}
+	*/
 }
